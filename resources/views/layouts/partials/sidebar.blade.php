b5e83fb3c0839d864421a4b4f08ef7130d7f451b
Zibu <kabuyajunior082@gmail.com>
2024-03-03 05:13:49 +0100

    ✨feature: publish vendor pagination

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -9,27 +9,7 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        @admin
-            <li class="nav-header user-panel pb-2 mb-2 d-flex" style="color: #4b646f;">GESTION BOISSON</li>
-            <li class="nav-item">
-                <a href="" class="nav-link">
-                    <i class="fas fa-glass-cheers"></i>
-                    <p>
-                    Boissons
-                    <span class="right badge badge-danger">Toute</span>
-                  </p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="" class="nav-link">
-                    <i class="fas fa-dumpster"></i>
-                    <p>
-                    Categorie
-                    <span class="right badge badge-success">Type</span>
-                  </p>
-                </a>
-            </li>
-        @endadmin
+        @include('layouts.partials.sidebar.admin')
 
         @if ((auth()->user()->role->id == 2))
             <li class="nav-item has-treeview">
---
+++ resources/views/layouts/partials/sidebar/admin.blade.php
@@ -0,0 +1,21 @@
+@admin
+<li class="nav-header user-panel pb-2 mb-2 d-flex" style="color: #4b646f;">GESTION BOISSON</li>
+<li class="nav-item">
+    <a href="{{ route('admin.products.index') }}" class="nav-link">
+        <i class="fas fa-glass-cheers"></i>
+        <p>
+        Boissons
+        <span class="right badge badge-danger">Toute</span>
+      </p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="" class="nav-link">
+        <i class="fas fa-dumpster"></i>
+        <p>
+        Categorie
+        <span class="right badge badge-success">Type</span>
+      </p>
+    </a>
+</li>
+@endadmin
