908c2ff3f6a73c56e3baff721b64f86549ee0d7f
Zibu <kabuyajunior082@gmail.com>
2024-03-07 02:04:46 +0100

    add product local

---
+++ resources/lang/en/products.php
@@ -0,0 +1,14 @@
+<?php
+
+    return [
+        'fields' => [
+            'type_id' => 'Type',
+            'description' => 'Description',
+            'quantite' => 'Quantity',
+            'purchase_price_per_locker' => 'Purchase price per locker',
+            'sale_price_per_locker' => 'Sale price per locker',
+            'purchase_price_per_bottle' => 'Purchase price per bottle',
+            'selling_price_per_bottle' => 'Selling price per bottle',
+        ],
+    ];
+
---
+++ resources/lang/fr/products.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'fields' => [
+        'type_id' => 'Type',
+        'description' => 'Description',
+        'quantite' => 'Quantité',
+        'purchase_price_per_locker' => 'Prix d\'achat par casier',
+        'sale_price_per_locker' => 'Prix de vente par casier',
+        'purchase_price_per_bottle' => 'Prix d\'achat par bouteille',
+        'selling_price_per_bottle' => 'Prix de vente par bouteille',
+    ],
+];
